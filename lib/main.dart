9b60adb12a6c3f6a9dc1dcc7bc2f5eb87dea2a3e
kaif8936 <55473516+kaif8936@users.noreply.github.com>
2021-02-03 22:44:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,151 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> dropdownItems = ['Male', 'Female'];
+
+  String _dropdownValue;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Bottom Sheet',
+      title: 'Material App',
       home: Scaffold(
-        body: Home(),
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ButtonBar(
+                buttonHeight: 1,
+                buttonMinWidth: 1,
+                alignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Never Mind',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    elevation: 5,
+                    padding: EdgeInsets.all(15),
+                    color: Colors.black,
+                    hoverColor: Colors.blue,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        3,
+                      ),
+                    ),
+                    child: Text(
+                      'Confirm Revoke',
+                      style: TextStyle(
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.black,
+                padding: EdgeInsets.all(10),
+                child: DropdownButton(
+                  hint: Text(
+                    'Geder',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  value: _dropdownValue,
+                  icon: Icon(
+                    Icons.arrow_drop_down,
+                    color: Colors.white,
+                  ),
+                  dropdownColor: Colors.black,
+                  elevation: 0,
+                  underline: Container(),
+                  onChanged: (String newValue) {
+                    setState(
+                      () {
+                        _dropdownValue = newValue;
+                      },
+                    );
+                  },
+                  items: dropdownItems.map(
+                    (value) {
+                      return DropdownMenuItem(child: Text(value), value: value);
+                    },
+                  ).toList(),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text('CLICK ME'),
+              ),
+              Ink(
+                decoration: ShapeDecoration(
+                  color: Colors.grey,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              OutlineButton(
+                onPressed: () {},
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 0.8,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Text('PHOTOS'),
+              ),
+              RaisedButton(
+                elevation: 5,
+                padding: EdgeInsets.all(15),
+                color: Color(0xff0000ff),
+                hoverColor: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                onPressed: () {},
+                child: Text('WATCH'),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+          ),
+          backgroundColor: Colors.amber,
+          onPressed: () {},
+        ),
       ),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                'https://image.freepik.com/free-vector/summer-background-with-beach-view_23-2147821949.jpg',
-              ),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(top: 700, left: 30),
-          height: 70,
-          width: 70,
-          child: FloatingActionButton(
-            backgroundColor: Colors.black,
-            child: Icon(
-              Icons.album,
-            ),
-            onPressed: () {
-              showModalBottomSheet(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(35.0),
-                  ),
-                ),
-                context: context,
-                builder: (BuildContext context) {
-                  return Container(
-                    padding: EdgeInsets.all(25),
-                    height: 400,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Surfing',
-                          style: GoogleFonts.courierPrime(
-                            textStyle: TextStyle(
-                              fontWeight: FontWeight.w900,
-                              fontSize: 40,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          '6 Calsses With sustainable topic and example',
-                          style: GoogleFonts.courierPrime(
-                            textStyle: TextStyle(
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Surfing is an habit to explore with your moving ability towards the water. Most importantly it is helpful to keep breath and to fresh mind',
-                          style: GoogleFonts.courierPrime(
-                            textStyle: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 27,
-                        ),
-                        RaisedButton(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(13.0),
-                          ),
-                          elevation: 0,
-                          textColor: Colors.white,
-                          padding: const EdgeInsets.all(0.0),
-                          child: Container(
-                            color: Colors.lightBlue.withOpacity(0.7),
-                            height: 60,
-                            width: double.infinity,
-                            child: Container(
-                              child: Center(
-                                child: Text(
-                                  'CLOSE',
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ),
-                            ),
-                          ),
-                          onPressed: () => Navigator.pop(context),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-        )
-      ],
-    );
-  }
-}
 
