0cb0a04d66c892af5aa40b174619339afd3892de
kaif8936 <55473516+kaif8936@users.noreply.github.com>
2021-02-03 22:55:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,150 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> dropdownItems = ['Male', 'Female'];
-
-  String _dropdownValue;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
+        backgroundColor: Color(0xff0A0026),
         body: Center(
-          child: Column(
-            children: [
-              ButtonBar(
-                buttonHeight: 1,
-                buttonMinWidth: 1,
-                alignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Never Mind',
-                      style: TextStyle(
-                        fontSize: 12,
-                      ),
-                    ),
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    elevation: 5,
-                    padding: EdgeInsets.all(15),
-                    color: Colors.black,
-                    hoverColor: Colors.blue,
-                    textColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        3,
-                      ),
-                    ),
-                    child: Text(
-                      'Confirm Revoke',
-                      style: TextStyle(
-                        fontSize: 13,
-                      ),
-                    ),
-                  ),
-                ],
+          child: Container(
+            padding: EdgeInsets.all(30),
+            child: TextField(
+              cursorColor: Colors.white,
+              style: TextStyle(
+                color: Color(0xffD0B9DB),
               ),
-              Container(
-                color: Colors.black,
-                padding: EdgeInsets.all(10),
-                child: DropdownButton(
-                  hint: Text(
-                    'Geder',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  style: TextStyle(
+              decoration: InputDecoration(
+                labelText: 'EMAIL',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                prefixIcon: Icon(
+                  Icons.lock_open,
+                  color: Colors.white,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
                     color: Colors.white,
                   ),
-                  value: _dropdownValue,
-                  icon: Icon(
-                    Icons.arrow_drop_down,
+                  borderRadius: BorderRadius.circular(
+                    12,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
                     color: Colors.white,
                   ),
-                  dropdownColor: Colors.black,
-                  elevation: 0,
-                  underline: Container(),
-                  onChanged: (String newValue) {
-                    setState(
-                      () {
-                        _dropdownValue = newValue;
-                      },
-                    );
-                  },
-                  items: dropdownItems.map(
-                    (value) {
-                      return DropdownMenuItem(child: Text(value), value: value);
-                    },
-                  ).toList(),
-                ),
-              ),
-              FlatButton(
-                onPressed: () {},
-                child: Text('CLICK ME'),
-              ),
-              Ink(
-                decoration: ShapeDecoration(
-                  color: Colors.grey,
-                  shape: CircleBorder(),
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.favorite,
-                    color: Colors.white,
+                  borderRadius: BorderRadius.circular(
+                    12,
                   ),
                 ),
               ),
-              OutlineButton(
-                onPressed: () {},
-                borderSide: BorderSide(
-                  color: Colors.black,
-                  width: 0.8,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Text('PHOTOS'),
-              ),
-              RaisedButton(
-                elevation: 5,
-                padding: EdgeInsets.all(15),
-                color: Color(0xff0000ff),
-                hoverColor: Colors.blue,
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(3),
-                ),
-                onPressed: () {},
-                child: Text('WATCH'),
-              ),
-            ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
-          ),
-          backgroundColor: Colors.amber,
-          onPressed: () {},
         ),
       ),
     );
   }
 }
+
 
 
